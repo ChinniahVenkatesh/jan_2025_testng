ac09806b4a74c67a00e29c9fefe0c9a8105f5d97
chinniah venkatesh <chinniahvenkatest>
2025-01-21 07:22:14 +0530

    secound push

--- src/test/java/TestScript/TestScript002.java
+++ src/test/java/TestScript/TestScript002.java
@@ -3,6 +3,7 @@ package TestScript;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 import org.testng.annotations.Test;
 
@@ -21,5 +22,13 @@ public class TestScript002 extends testBase{
 		sdf.applyPattern("MM_dd_yyyy_HH_mm_ss");
 		sdf.format(d);
 		System.out.println(sdf.format(d));
+	}
+	
+	
+	@Test
+	public void TC005()
+	{
+		String password = "Password@12345";
+		System.out.println(password);
 	}
 }
